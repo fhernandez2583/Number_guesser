a47a6a50e456f0ac140b8f0af0ec075508456e00
Macbook-air <40375223+fhernandez2583@users.noreply.github.com>
2025-03-17 14:24:26 -0500

    complete functionality of game

--- script.js
+++ script.js
@@ -4,3 +4,25 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+const generateTarget = () => {
+    return Math.floor(Math.random() * 10)
+}
+// console.log(generateTarget());
+
+const compareGuesses = (humanGuess, computerGuess, targetGuess) => {
+    const humanDifference = Math.abs(targetGuess - humanGuess);
+    const computerDifference = Math.abs(targetGuess - computerGuess);
+    return humanDifference <= computerDifference;
+}
+
+const updateScore = winner => {
+    if (winner === 'human') {
+        return humanScore++;
+    } else if (winner = 'computer') {
+        return computerScore++;
+    }
+}
+
+const advanceRound = () => {
+    return currentRoundNumber++;
+}
